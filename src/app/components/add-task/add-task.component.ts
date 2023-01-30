ffd90d70717fcd05b243b4263ac251e00f953305
mokumus <rocketpolka@gmail.com>
2023-01-30 14:54:35 +0300

    feat: ui service

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -12,7 +14,12 @@ export class AddTaskComponent {
   text!:string;
   day!: string;
   reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription: Subscription;
 
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
   onSubmit(): void{
     if(!this.text) {
       alert('Please add a task!');
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Angular-Task-Tracker';
+  title: string = 'Task Tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
     //throw new Error('Method not implemented.');
   }
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
